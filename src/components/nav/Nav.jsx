fd46e18f01e434557e985ef4ecc7e236531e7565
fredeskrubba <frederikskrubbeltrang@outlook.dk>
2023-06-03 14:11:27 +0200

    add project html + styling

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,16 +1,16 @@
-import githubIcon from "../../assets/icons/github-icon.svg"
-import linkedIcon from "../../assets/icons/linkedin-icon.svg"
-import burgerIcon from "../../assets/icons/burger-icon.svg"
+import { ReactComponent as GithubIcon} from "../../assets/icons/github-icon.svg"
+import {ReactComponent as LinkedIcon} from "../../assets/icons/linkedin-icon.svg"
+import {ReactComponent as BurgerIcon } from "../../assets/icons/burger-icon.svg"
 
 const Nav = () => {
   return (
     <nav>
       <section className='mobile-nav'>
         <div className='socials'>
-          <a href="https://www.linkedin.com/in/frederik-skrubbeltrang-698b4a1a3/" rel="noreferrer" target="_blank"><img src={linkedIcon} alt="linkedin-icon"/></a>
-          <a href="https://github.com/fredeskrubba" rel="noreferrer" target="_blank"><img src={githubIcon} alt="github-icon" /></a>
+          <a href="https://www.linkedin.com/in/frederik-skrubbeltrang-698b4a1a3/" rel="noreferrer" target="_blank"><LinkedIcon/></a>
+          <a href="https://github.com/fredeskrubba" rel="noreferrer" target="_blank"><GithubIcon/></a>
         </div>
-        <img src={burgerIcon} alt="menu" />
+        <BurgerIcon/>
       </section>
     </nav>
   )
---
+++ src/components/projects/Project.jsx
@@ -0,0 +1,27 @@
+import { ReactComponent as GithubLogo} from "../../assets/icons/github-icon.svg"
+import {ReactComponent as RedirectLogo} from "../../assets/icons/redirect-icon.svg"
+
+const Project = ({title, img, description, techs, alt}) => {
+  return (
+    <div className='project'>
+        <img src={img} alt={alt} />
+        <article className="project-info">
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </article>
+        <article className="bottom-bar">
+            <div className='technologies'>
+                {
+                    techs.map((tech) => <p key={1} className="technology"> {tech}</p>)
+                }
+            </div>
+            <div className="project-buttons">
+               <GithubLogo className="project-button"/>
+               <RedirectLogo className="project-button"/> 
+            </div>
+        </article>
+    </div>
+  )
+}
+
+export default Project
