21035ecaeac0af904784d60a0704bd963bde8b97
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2023-06-07 14:22:42 +0200

    add contact styling and html + mobile footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import Nav from "./components/nav/Nav"
 import Header from "./components/header/Header"
 import Projects from "./components/projects/Projects"
 import About from "./components/about/About"
+import Contact from "./components/contact/Contact"
+import {ReactComponent as GithubLogo} from "./assets/icons/github-icon.svg"
+import {ReactComponent as LinkedinLogo} from "./assets/icons/linkedin-icon.svg"
+
 function App() {
   
   return (
@@ -10,6 +14,17 @@ function App() {
       <Header/>
       <Projects/>
       <About/>
+      <Contact/>
+      <footer className='mobile-footer'>
+            <div className="social-container">
+                <GithubLogo/>
+                <h4>Github/fredeskrubba</h4>
+            </div>
+            <div className="social-container">
+                <LinkedinLogo/>
+                <h4>Frederik Skrubbeltrang</h4>
+            </div>
+        </footer>
     </>
   )
 }
