7fbb9b46c6b121bb9b5bf1968c31c1305f73a6fc
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2023-06-08 14:43:08 +0200

    add resume

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,8 @@ const Header = () => {
   return (
     <section className='header'>
         <h1>Frederik Skrubbeltrang</h1>
-        <p className='resume'>Se CV</p>   
+        <a href="https://drive.google.com/file/d/14yXKu4yylgrQnfjWsW8riN_-gP8WQxn9/view?usp=sharing" rel="noreferrer" target='_blank'><p className='resume'>Se CV</p>   </a>
+        
     </section>
   )
 }
