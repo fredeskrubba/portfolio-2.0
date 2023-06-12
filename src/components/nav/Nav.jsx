aa089b26addd0aa44ca8ada8ea5d967eefdd6a8d
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2023-06-12 14:57:39 +0200

    add desktop nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -12,6 +12,17 @@ const Nav = () => {
         </div>
         <BurgerIcon/>
       </section>
+      <section className="desktop-nav">
+        <article className="nav-links">
+          <a href="/">Projekter</a>
+          <a href="/">Om Mig</a>
+          <a href="/">Kontakt</a>
+        </article>
+        <article className="socials">
+          <a href="https://www.linkedin.com/in/frederik-skrubbeltrang-698b4a1a3/" rel="noreferrer" target="_blank"><LinkedIcon/></a>
+          <a href="https://github.com/fredeskrubba" rel="noreferrer" target="_blank"><GithubIcon/></a>
+        </article>
+      </section>
     </nav>
   )
 }
