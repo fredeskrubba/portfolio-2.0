bdabac91f1bf10851633ce1ca94f4874c8a85087
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2023-06-17 10:31:15 +0200

    add desktop styling

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -63,12 +63,22 @@ const About = () => {
           <h2>Om Mig</h2>
           <img src={arrow} alt="arrow" />
         </article>
-        <img src={picture} alt="selfie" className='about-picture'/>
-        <article className='about-intro'>
-            <h2>Hej! Jeg er Frederik</h2>
-            <p>Som passioneret Webudvikler, med flair for design har jeg ikke bare kendskab til diverse frontend-teknologier, men også gå-på-mod til at lære det der skulle mangle. Denne side er foreksempelt designet, kodet og opsat af mig selv. </p>
-            <p>Det var lidt om mig, Men hvilke teknologier kan jeg bruge?</p>
-        </article>
+        <section className="mobile-about">
+            <img src={picture} alt="selfie" className='about-picture'/>
+            <article className='about-intro'>
+                <h2>Hej! Jeg er Frederik</h2>
+                <p>Som passioneret Webudvikler, med flair for design har jeg ikke bare kendskab til diverse frontend-teknologier, men også gå-på-mod til at lære det der skulle mangle. Denne side er foreksempelt designet, kodet og opsat af mig selv. </p>
+                <p>Det var lidt om mig, Men hvilke teknologier kan jeg bruge?</p>
+            </article>
+        </section>
+        <section className='desktop-about'>
+            <img src={picture} alt="selfie" className='about-picture'/>
+            <article className='about-intro-desktop'>
+                <h2>Hej! Jeg er Frederik</h2>
+                <p>Som passioneret Webudvikler, med flair for design har jeg ikke bare kendskab til diverse frontend-teknologier, men også gå-på-mod til at lære det der skulle mangle. Denne side er foreksempelt designet, kodet og opsat af mig selv. </p>
+                <p>Det var lidt om mig, Men hvilke teknologier kan jeg bruge?</p>
+            </article>
+        </section>
         <figure className='skill-grid'>
             {
                 skills.slice(0, skillsShown).map((skill) => <Skill key={skill.id} Logo={skill.logo} name={skill.name}/>)
