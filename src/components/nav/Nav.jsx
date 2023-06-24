5a22a5af0aba772cd14b0e96419eb5a5a77f3558
Frederik Skrubbeltrang <frederikskrubbeltrang@outlook.dk>
2023-06-24 19:26:21 +0200

    add scroll nav function

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,11 @@
 import { ReactComponent as GithubIcon} from "../../assets/icons/github-icon.svg"
 import {ReactComponent as LinkedIcon} from "../../assets/icons/linkedin-icon.svg"
 import {ReactComponent as BurgerIcon } from "../../assets/icons/burger-icon.svg"
+import * as Scroll from 'react-scroll';
 
 const Nav = () => {
+
+  let Link = Scroll.Link
   return (
     <nav>
       <section className='mobile-nav'>
@@ -14,9 +17,9 @@ const Nav = () => {
       </section>
       <section className="desktop-nav">
         <article className="nav-links">
-          <a href="/">Projekter</a>
-          <a href="/">Om Mig</a>
-          <a href="/">Kontakt</a>
+          <Link  smooth={true} to="projects" offset={-25} spy={true}>Projekter</Link>
+          <Link  smooth={true} to="about" offset={-25} spy={true}>Om Mig</Link>
+          <Link  smooth={true} to="contact">Kontakt</Link>
         </article>
         <article className="socials">
           <a href="https://www.linkedin.com/in/frederik-skrubbeltrang-698b4a1a3/" rel="noreferrer" target="_blank"><LinkedIcon/></a>
